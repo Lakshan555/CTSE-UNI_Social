564519aaa0e26203e19bd9cf1aa08ea4684b3b07
Lakshan-IT19951836 <onlineipl18@gmail.com>
2023-03-17 22:18:33 +0530

    App js changes

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigator from "./app/navigation/TabNavigator";
+import { createNativeStackNavigator } from "react-native-screens/native-stack";
+const Stack = createNativeStackNavigator();
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { store } from "./app/store/index";
+import Login from "./app/components/LoginScreen";
+import SplashScreen from "./app/components/SplashScreen";
+
+const MyStack = () => {
+  return (
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen
+        options={{ headerShown: false }}
+        component={SplashScreen}
+        name="SplashScreen"
+      />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        component={Login}
+        name="Login"
+      />
+      <Stack.Screen
+        name="tabNavigator"
+        options={{ headerShown: false }}
+        component={TabNavigator}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        options={{ headerShown: false }}
+        component={SplashScreen}
+        name="SplashScreen"
+      />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        component={Login}
+        name="Login"
+      />
+    </Stack.Navigator>
+  );
+};
+
+const RootNavigation = () => {
+  const token = useSelector((state) => state.AuthReducers.authToken);
+  console.log("authToken", token);
+
+  return (
+    <NavigationContainer>
+      {token === null ? <AuthStack /> : <MyStack />}
+    </NavigationContainer>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <RootNavigation />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
