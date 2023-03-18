83c431ce8b286eab82b49e49245e85fce81dd067
Lakshan-IT19951836 <onlineipl18@gmail.com>
2023-03-18 08:38:06 +0530

    create home page structure

---
+++ app/components/Home.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+ 
+} from "react-native";
+import Header from "./Common/Header";
+
+const Home = ({ navigation }) => {
+
+  return (
+    <View style={styles.screen}>
+      <Header prop1="sss"/>
+      <View style={styles.body}>
+        
+      </View>
+        
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  body: {
+    flex: 8,
+    backgroundColor: "#14141405",
+    padding: 10,
+    width: "100%",
+  },
+});
+
+export default Home;
