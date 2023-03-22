ec5bf893fa5a610b262c22f444bf3243e505ef26
lakshika <lakshikamedhavini@gmail.com>
2023-03-22 22:38:17 +0530

    market input ui created

---
+++ app/Screens/MyMarketItems.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MyMarketItems = () => {
+  return (
+    <View>
+      <Text>MyMarketItems</Text>
+    </View>
+  );
+};
+
+export default MyMarketItems;
+
+const styles = StyleSheet.create({});
--- app/navigation/MarketPlaceNavigation.js
+++ app/navigation/MarketPlaceNavigation.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { createNativeStackNavigator } from "react-native-screens/native-stack";
+import AddNewMarketItem from "../Screens/AddNewMarketItem";
 import MarketPlace from "../Screens/MarketPlace";
 import MarketPlaceViewDetails from "../Screens/MarketPlaceViewDetails";
+import MyMarketItems from "../Screens/MyMarketItems";
 const Stack = createNativeStackNavigator();
 
 function MarketPlaceNavigation() {
@@ -25,6 +27,24 @@ function MarketPlaceNavigation() {
         }}
         name="MarketPlaceViewDetails"
         component={MarketPlaceViewDetails}
+      />
+      <Stack.Screen
+        options={{
+          title: "Add Market Item",
+          headerTintColor: "white",
+          headerStyle: { backgroundColor: "#39367e" },
+        }}
+        name="AddNewMarketItem"
+        component={AddNewMarketItem}
+      />
+      <Stack.Screen
+        options={{
+          title: "My Market Items",
+          headerTintColor: "white",
+          headerStyle: { backgroundColor: "#39367e" },
+        }}
+        name="MyMarketItems"
+        component={MyMarketItems}
       />
     </Stack.Navigator>
   );
