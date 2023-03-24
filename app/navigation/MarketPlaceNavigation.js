2873e228bb7676d4eb1b73473aafcfdf08349d30
pasindubhasura <srmpasindubhasura@gmail.com>
2023-03-24 08:28:16 +0530

    refactored

--- app/navigation/MarketPlaceNavigation.js
+++ app/navigation/MarketPlaceNavigation.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createNativeStackNavigator } from "react-native-screens/native-stack";
 import AddNewMarketItem from "../Screens/AddNewMarketItem";
 import MarketPlace from "../Screens/MarketPlace";
 import MarketPlaceViewDetails from "../Screens/MarketPlaceViewDetails";
