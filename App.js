a119fadf30e3311140c8628fb33212d757d3f26e
Lakshan-IT19951836 <onlineipl18@gmail.com>
2023-03-24 08:14:45 +0530

    post controller

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { Provider, useDispatch, useSelector } from "react-redux";
 import { store } from "./app/store/index";
 import Login from "./app/components/LoginScreen";
 import SplashScreen from "./app/components/SplashScreen";
+import blogDetails from "./app/Screens/Blog/blogDetails";
+import AddBlog from "./app/Screens/Blog/AddBlog";
 
 const MyStack = () => {
   return (
@@ -26,13 +28,29 @@ const MyStack = () => {
         options={{ headerShown: false }}
         component={TabNavigator}
       />
+      <Stack.Screen
+        name="blogDetails"
+        options={{ headerShown: false }}
+        component={blogDetails}
+      />
+       <Stack.Screen
+        name="blogForm"
+        options={{ headerShown: false }}
+        component={AddBlog}
+      />
     </Stack.Navigator>
   );
 };
 
 const AuthStack = () => {
   return (
+    
     <Stack.Navigator>
+        <Stack.Screen
+        name="tabNavigator"
+        options={{ headerShown: false }}
+        component={TabNavigator}
+      />
       <Stack.Screen
         options={{ headerShown: false }}
         component={SplashScreen}
@@ -42,6 +60,16 @@ const AuthStack = () => {
         options={{ headerShown: false }}
         component={Login}
         name="Login"
+      />
+       <Stack.Screen
+        name="blogDetails"
+        options={{ headerShown: false }}
+        component={blogDetails}
+      />
+       <Stack.Screen
+        name="blogForm"
+        options={{ headerShown: false }}
+        component={AddBlog}
       />
     </Stack.Navigator>
   );
